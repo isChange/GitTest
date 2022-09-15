648f5bd2a0b1ddf42371aaba767b027525f51a42
LyJava <ly@qq.com>
2022-09-15 18:04:44 +0800

    second commit version1.1

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,7 +2,8 @@ public class HelloGit {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
 
     }
 }
